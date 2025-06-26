ad349915dcf4fda872ec8ad002b7f2639b8820a8
Ishan Singh <ishan2004.official@gmail.com>
2025-06-26 17:17:08 +0530

    Commented out SeedSlugValueForExistingTasks migration

--- Gemfile
+++ Gemfile
@@ -7,7 +7,8 @@ ruby "3.3.5"
 gem "responders"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
-gem "rails", "~> 7.1.3", ">= 7.1.3.4"
+# gem "rails", "~> 7.1.3", ">= 7.1.3.4"
+gem "rails", "7.1.3.4"
 
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
--- db/migrate/20250607135442_seed_slug_value_for_existing_tasks.rb
+++ db/migrate/20250607135442_seed_slug_value_for_existing_tasks.rb
@@ -2,16 +2,16 @@
 
 class SeedSlugValueForExistingTasks < ActiveRecord::Migration[7.1]
   def up
-    Task.find_each do |task|
-      task.send(:set_slug)
-      task.save!(validate: false)
-    end
+    # Task.find_each do |task|
+    #   task.send(:set_slug)
+    #   task.save!(validate: false)
+    # end
   end
 
   def down
-    Task.find_each do |task|
-      task.slug = nil
-      task.save!(validate: false)
-    end
+    # Task.find_each do |task|
+    #   task.slug = nil
+    #   task.save!(validate: false)
+    # end
   end
 end
