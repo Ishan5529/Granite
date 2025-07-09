7c388fe4a8dea5765a79a0ab48a155fe2a7773e9
Vishal Yadav <Vishal.yadav2279@gmail.com>
2025-07-09 18:37:17 +0530

    Updated Health checks controller

--- app/controllers/health_checks_controller.rb
+++ app/controllers/health_checks_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class HealthChecksController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user_using_x_auth_token
 
   def index
-    render json: { status: 'ok', message: 'Application is alive' }, status: :ok
+    render json: { status: "ok", message: "Application is alive" }, status: :ok
   end
 end
